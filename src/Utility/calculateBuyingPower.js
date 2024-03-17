ab6eb20fbaaa7988c1306731bcecbe58503f03e0
null-zero <trueshrike7@gmail.com>
2024-03-16 19:34:53 -0500

    Fix calculateBuyingPower causing crash
    Early returns if the hand is empty

--- src/Utility/calculateBuyingPower.js
+++ src/Utility/calculateBuyingPower.js
@@ -2,6 +2,8 @@ import { cards } from "../Objs/Cards";
 
 export function calculateBuyingPower({ G, playerID }) {
     let buyingPower = 0;
+    if (G.players[playerID].hand.length === 0) return buyingPower;
+    
     G.players[playerID].hand.forEach((card) => {
         if (cards[card].type.includes("treasure")) {
             buyingPower += cards[card].coins;
